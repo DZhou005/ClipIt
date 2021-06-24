e58ec4ea1c38fa1b13dd79c3f1f0c5aff2c33957
DZhou005 <74795454+DZhou005@users.noreply.github.com>
2021-06-24 13:28:35 -0700

    updates to navbar images and centering to comments

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
-import pic from '../../images/clipit_2-1.png'
+import pic from '../../images/clipit.png'
 import './NavBar.css'
 
 
 const NavBar = () => {
   const loggedInUser = useSelector(state => state.session.user.username)
-               
+
 
   return (
     <nav>
